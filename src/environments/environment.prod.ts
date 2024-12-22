84cef1f03bf3767fc41311c2645154849bd282a1
nithinta <nithin.ta@gmail.com>
2024-12-23 02:18:00 +0530

    Tes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
 
   APP_WELCOME: 'http://http://3.6.170.253:4202/login.html',
   APP_DASHBOARD: 'http://http://3.6.170.253:4202/dashboard.html',
-  AUTH_BEGIN: 'http://162.241.127.138/goldenage/auth/begin',
-  API_ENDPOINT: 'http://162.241.127.138/goldenage/server.php/api/v1/',
+  AUTH_BEGIN: 'http://3.6.170.253:4202/goldenage/auth/begin',
+  API_ENDPOINT: 'http://3.6.170.253:4202/goldenage/server.php/api/v1/',
   CHAT_API_ENDPOINT: 'http://162.241.127.138:8102/',
   LOBBY_WEB_URL: 'http://162.241.127.138:48001/web',
   CHAT_SOCKET_URL: 'ws://162.241.127.138:8102/websocket',
