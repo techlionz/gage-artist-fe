1cabeb56096f6e27ab690648c2414e6144cf832b
nithinta <nithin.ta@gmail.com>
2024-12-23 04:28:50 +0530

    app url fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
 
-  APP_WELCOME: 'http://http://3.6.170.253:4202/login.html',
-  APP_DASHBOARD: 'http://http://3.6.170.253:4202/dashboard.html',
-  AUTH_BEGIN: 'http://3.6.170.253:4202/goldenage/auth/begin',
-  API_ENDPOINT: 'http://3.6.170.253:4202/goldenage/server.php/api/v1/',
-  CHAT_API_ENDPOINT: 'http://162.241.127.138:8102/',
-  LOBBY_WEB_URL: 'http://162.241.127.138:48001/web',
-  CHAT_SOCKET_URL: 'ws://162.241.127.138:8102/websocket',
+  APP_WELCOME: 'http://3.6.170.253:4202/login.html',
+  APP_DASHBOARD: 'http://3.6.170.253:4202/dashboard.html',
+  AUTH_BEGIN: 'http://3.6.170.253:1080/server.php/auth/begin',
+  API_ENDPOINT: 'http://3.6.170.253:1080/server.php/api/v1/',
+  CHAT_API_ENDPOINT: 'http://3.6.170.253:8102/',
+  LOBBY_WEB_URL: 'http://13.6.170.253:48001/web',
+  CHAT_SOCKET_URL: 'ws://3.6.170.253:8102/websocket',
 };
 
