9ef44d29f8e971b7e3e4c02f6b83ef3d3fd63ca8
Nithin <gamegooda@gmail.com>
2025-06-03 00:24:44 +0530

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
 
-  APP_WELCOME: 'http://3.6.170.253:4202/login.html',
-  APP_DASHBOARD: 'http://3.6.170.253:4202/dashboard.html',
+  APP_WELCOME: 'https://kyc.pokerpad.net/login.html',
+  APP_DASHBOARD: 'https://kyc.pokerpad.net/dashboard.html',
   AUTH_BEGIN: 'http://3.6.170.253:1080/server.php/auth/begin',
-  API_ENDPOINT: 'http://3.6.170.253:1080/server.php/api/v1/',
+  API_ENDPOINT: 'https://api.pokerpad.cm/api/v1/',
   CHAT_API_ENDPOINT: 'http://3.6.170.253:8102/',
   LOBBY_WEB_URL: 'http://13.6.170.253:48001/web',
   CHAT_SOCKET_URL: 'ws://3.6.170.253:8102/websocket',
