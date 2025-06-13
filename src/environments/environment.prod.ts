9a083fb985c2a450a9373e3e29412cb1d2cc8f38
arunak007-sys <arun2017ak@gmail.com>
2025-06-13 12:58:49 +0530

    servre url chnaged

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
 
   APP_WELCOME: 'http://3.6.170.253:4202/login.html',
   APP_DASHBOARD: 'http://3.6.170.253:4202/dashboard.html',
-  AUTH_BEGIN: 'http://3.6.170.253:1080/server.php/auth/begin',
-  API_ENDPOINT: 'http://3.6.170.253:1080/server.php/api/v1/',
+  AUTH_BEGIN: 'http://3.6.170.253:1080/server.php/auth/begin', 
+  API_ENDPOINT: 'http://3.6.170.253:1080/server.php/api/v1/', 
   CHAT_API_ENDPOINT: 'http://3.6.170.253:8102/',
   LOBBY_WEB_URL: 'http://13.6.170.253:48001/web',
   CHAT_SOCKET_URL: 'ws://3.6.170.253:8102/websocket',
