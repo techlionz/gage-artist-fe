e1f834c3ccc6d3e5aa645ddee1486230b152fdf9
arunak007-sys <arun2017ak@gmail.com>
2025-07-05 16:04:01 +0530

    add env local, staging, and prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,15 @@
 export const environment = {
   production: true,
 
-  APP_WELCOME: 'http://3.6.170.253:4202/login.html',
-  APP_DASHBOARD: 'http://3.6.170.253:4202/dashboard.html',
-  AUTH_BEGIN: 'http://3.6.170.253:1080/server.php/auth/begin', 
-  API_ENDPOINT: 'http://3.6.170.253:1080/api/v1/', 
+  APP_WELCOME: 'https://kyc.pokerpad.net/login.html',
+  APP_DASHBOARD: 'https://kyc.pokerpad.net/dashboard.html',
+  AUTH_BEGIN: 'http://3.6.170.253:1080/server.php/auth/begin',
+  API_ENDPOINT: 'https://api.pokerpad.com/api/v1/',
   CHAT_API_ENDPOINT: 'http://3.6.170.253:8102/',
   LOBBY_WEB_URL: 'http://13.6.170.253:48001/web',
   CHAT_SOCKET_URL: 'ws://3.6.170.253:8102/websocket',
 };
+
+
+
 
