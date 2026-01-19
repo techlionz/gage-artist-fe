98390e19aa749af54be7de634ddac2d65bdd8ce0
arunak007-sys <arun2017ak@gmail.com>
2026-01-19 17:33:18 +0530

    code comment

--- src/app/auth/service/authy.service.ts
+++ src/app/auth/service/authy.service.ts
@@ -11,18 +11,18 @@ export class AuthyService {
       let token = localStorage.getItem('userData') ?? ''; // get token from local storage
       if (token.length > 0) {
         try {
-			// Attempt to parse the stringified JSON into a JavaScript object
+          // Attempt to parse the stringified JSON into a JavaScript object
           let payload = JSON.parse(token);
           let userId = payload.id; // convert payload into an Object
-		  // Check if the 'id' property exists and is not empty
+          // Check if the 'id' property exists and is not empty
           if (userId != '') {
             return true;
           }
-		  // Catch potential JSON parsing errors if 'userData' is corrupted
+          // Catch potential JSON parsing errors if 'userData' is corrupted
         } catch (e) {}
       }
     } catch (e) {}
-	// Default to false if any checks fail
+    // Default to false if any checks fail
     return false;
   }
 }
